52bfd512b0dbf6a2491df9e15fa84ba2de1de8e5
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2022-10-06 12:51:38 +0100

    Development (#10)
    * enable code coverage on test (#6)
    
    * Feature/setup (#7)
    
    * enable code coverage on test
    
    * update workflow to accomodate sonar cloud manual analysis
    
    * Feature/setup (#9)
    
    * enable code coverage on test
    
    * update workflow to accomodate sonar cloud manual analysis
    
    * ad angular material
    
    * append api to backend url
    
    * establish communication between frontend and backend
    
    * Feature/setup (#11)
    
    * enable code coverage on test
    
    * update workflow to accomodate sonar cloud manual analysis
    
    * ad angular material
    
    * append api to backend url
    
    * establish communication between frontend and backend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
+  backendUrl = 'http://localhost:3000/api';
+
+  constructor(private readonly http: HttpClient) {}
+
+  getTextResponse() {
+    return this.http.get(this.backendUrl, { responseType: 'text' }).pipe(
+      tap({
+        next: (data) => console.log(data),
+        error: (error) => console.log(error),
+      })
+    );
+  }
+
+  onclick() {
+    return this.getTextResponse().subscribe();
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialComponentsModule } from './modules/material-components.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialComponentsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
