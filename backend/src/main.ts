350e6fd8a9423d9a6e55ff96474cccd12f8960a0
ola <raymond.oyinlola@gmail.com>
2022-10-06 16:55:24 +0800

    append api to backend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
