fc39479512c394df08a50bff0001250ae9d22633
ola <raymond.oyinlola@gmail.com>
2022-10-21 10:21:51 +0800

    remove white lines

--- backend/src/infrastructure/data_access/repositories/schemas/location.schema.ts
+++ backend/src/infrastructure/data_access/repositories/schemas/location.schema.ts
@@ -24,7 +24,7 @@ export class LocationData extends BaseDocument implements ILocation {
 
   @Prop({ type: String, required: true })
   state: string;
-  
+
   @Prop({ type: String })
   latitude?: string;
 
