398a4c8921939146b2b08927c3ee091a84aae796
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2023-01-19 23:30:08 +0800

    Auth (#135)
    * refactor auth reducer loginuser method, abstract storage of user info to another method
    
    * Fix the authguard issues
    
    * update the auth flow
    
    * create item and menu schema and their corresponding domain classes
    
    * fix failing test
    
    * create item repository, module, services and schema
    
    * create item controller
    
    * fix failing tests
    
    * create the get items service
    
    * create the menu repository and menu mapper

--- backend/src/infrastructure/data_access/repositories/schemas/menu.schema.ts
+++ backend/src/infrastructure/data_access/repositories/schemas/menu.schema.ts
@@ -1,10 +1,11 @@
 import { Type } from 'class-transformer';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import { BaseDocument } from 'src/infrastructure/database';
 import { IMenuData } from './../interfaces/menu-model.interface';
 import { ItemDataModel } from './item.schema';
 
+export type MenuDocument = MenuDataModel & Document;
 @Schema({ versionKey: 'false' })
 export class MenuDataModel extends BaseDocument implements IMenuData {
   @Prop({ type: String, required: true })
@@ -13,7 +14,7 @@ export class MenuDataModel extends BaseDocument implements IMenuData {
   @Prop({ type: String })
   description: string;
 
-  @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: ItemDataModel }])
+  @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: ItemDataModel.name }])
   @Type(() => ItemDataModel)
   items: ItemDataModel[];
 }
---
+++ backend/src/menu/menu.mapper.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from '../item/item';
+import { AuditMapper } from './../audit/audit.mapper';
+import { IMapper } from './../domain/mapper/mapper';
+import { ItemDataModel } from './../infrastructure/data_access/repositories/schemas/item.schema';
+import { MenuDataModel } from './../infrastructure/data_access/repositories/schemas/menu.schema';
+import { ItemMapper } from './../item/item.mapper';
+import { Menu } from './menu';
+
+@Injectable()
+export class MenuMapper implements IMapper<Menu, MenuDataModel> {
+  constructor(private readonly auditMapper: AuditMapper, private readonly itemMapper: ItemMapper) {}
+  toPersistence(entity: Menu): MenuDataModel {
+    const { items, name, description, audit } = entity;
+    const {
+      auditCreatedBy,
+      auditCreatedDateTime,
+      auditModifiedBy,
+      auditModifiedDateTime,
+      auditDeletedBy,
+      auditDeletedDateTime,
+    } = audit;
+    let mappedItems: ItemDataModel[] = [];
+    if (items && items.length) {
+      mappedItems = items.map((item) => this.itemMapper.toPersistence(item));
+    }
+    const menuDocument: MenuDataModel = {
+      name,
+      description,
+      items: mappedItems,
+      auditCreatedBy,
+      auditCreatedDateTime,
+      auditModifiedBy,
+      auditModifiedDateTime,
+      auditDeletedDateTime,
+      auditDeletedBy,
+    };
+    return menuDocument;
+  }
+
+  toDomain(model: MenuDataModel): Menu {
+    const { _id, items, name, description } = model;
+    let itemsToDomain: Item[] = [];
+    if (items && items.length) {
+      itemsToDomain = items.map((item) => this.itemMapper.toDomain(item));
+    }
+    const entity: Menu = Menu.create(
+      { items: itemsToDomain, name, description, audit: this.auditMapper.toDomain(model) },
+      _id,
+    ).getValue();
+    return entity;
+  }
+}
