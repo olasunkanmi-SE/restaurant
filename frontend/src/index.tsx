29d039aea0fcbfa149ba8cfb12f8350b37676e28
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2023-02-06 14:56:30 +0800

    Feature/UI (#174)
    * scaffold react js
    
    * create the header component
    
    * update github workflow for frontend CI
    
    * update github workflow for frontend CI
    
    * replace semantic ui for react bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
