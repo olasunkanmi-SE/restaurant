91605c4db793fc065790362b13ac49dd2d4f5b3d
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2023-03-11 20:09:08 +0800

    Feature/refactor (#224)
    * create the get menubyId method
    
    * remove deleted addons and items from menu
    
    * resolve conflicts
    
    * remove code smells
    
    * update menu update service and repository to accomodate itemIds and AddonIds
    
    * jsutify the content of the application to the center
    
    * update the hope page to display a list of menu
    
    * create the menu component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import { Col, Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
-import { Menu } from "./components";
+import { Navigation } from "./components";
 import { ShoppingCartProvider } from "./contexts/shoppingCartContext";
-import { About, Home, Store } from "./pages";
+import { About, Home, Menu } from "./pages";
 function App() {
   return (
     <ShoppingCartProvider>
       <Container fluid="md">
         <section className="h-100 d-flex align-items-center justify-content-center">
           <Col xs={12} md={12} lg={6} className="mb-3 ">
-            <Menu />
+            <Navigation />
             <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="/about" element={<About />}></Route>
-              <Route path="/store" element={<Store />}></Route>
+              <Route path="/menu" element={<Menu />}></Route>
             </Routes>
           </Col>
         </section>
--- frontend/src/pages/Store.tsx
+++ frontend/src/pages/Store.tsx
@@ -1,15 +1,3 @@
-import { Fragment } from "react";
-import { StoreItem } from "../components";
-
 export const Store = () => {
-  return (
-    <Fragment>
-      <StoreItem
-        name="Jollof Rice and fish"
-        description="This is a tradition west african food"
-        imageUrl="https://i0.wp.com/travelandmunchies.com/wp-content/uploads/2022/11/IMG_8133-scaled.jpg?fit=2560%2C1828&ssl=1"
-        basePrice={5}
-      />
-    </Fragment>
-  );
+  return;
 };
