1d5973f52a323325ece9d5ab75c153f3bb702bec
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2023-03-30 22:51:06 +0800

    Development (#252)
    * update github workflow yaml file
    
    * rename the frontend folder
    
    * create the checkout button
    
    * create dynamic route
    
    * update the routing definition and getmenubyId API call
    
    * Merge branch 'development' of github.com:olasunkanmi-SE/restaurant into development
    
    * remove the styling for menu name
    
    * optimize the components
    
    * conditionally render the checkout component
    
    * remove code smell
    
    * Merge branch 'release' into development
    
    * update the menu item page and clean up the navigation
    
    * create the delete menu repo method, service and controller
    
    * create the delete menu API
    
    * update food menu component to include item id
    
    * update food menu component to include item id
    
    * add the add menu item to cart functionality
    
    * implement add menu items to cart
    
    * fix build errors
    
    * fix code smell
    
    * create the item quantity button component
    
    * update the menu list ui

---
+++ frontend/src/components/MenuInfo.tsx
@@ -0,0 +1,52 @@
+import { CSSProperties } from "react";
+import { QtyButton } from "./addItemButton";
+
+type description = {
+  name: string;
+  description?: string;
+  price: number;
+};
+
+const nameStyle: CSSProperties = {
+  maxHeight: "3.6em",
+  textAlign: "center",
+  display: "flex",
+  justifyContent: "flex-start",
+  flexDirection: "column",
+  fontSize: "18.4px",
+  lineHeight: "17.28px",
+  flex: "0 0 auto",
+  fontWeight: 600,
+};
+
+const info: CSSProperties = {
+  position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  lineHeight: "1.2em",
+  zIndex: 99,
+  flex: "1 20 auto",
+  fontFamily: "Roboto Condensed",
+};
+
+const handleClick = () => {
+  return "Hello";
+};
+
+export const MenuInfo = ({ name, description, price }: description) => {
+  return (
+    <>
+      <div style={info} className="vstack gap-2 mt-3">
+        <div style={nameStyle}>{name}</div>
+        <div>{description}</div>
+        <div style={{ marginTop: "5px", marginBottom: "20px" }} className="hstack gap-2">
+          <div style={{ margin: "auto", fontWeight: 600 }}>RM {price}</div>
+          <div style={{ margin: "auto" }} className="ms-auto">
+            <QtyButton sign="increment" onClick={handleClick} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
