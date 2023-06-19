66672d9b14a692f709f033f5264d32dd9488a3ad
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2023-06-19 12:54:01 +0800

    Development (#310)
    * update github workflow yaml file
    
    * rename the frontend folder
    
    * create the checkout button
    
    * create dynamic route
    
    * update the routing definition and getmenubyId API call
    
    * Merge branch 'development' of github.com:olasunkanmi-SE/restaurant into development
    
    * remove the styling for menu name
    
    * optimize the components
    
    * conditionally render the checkout component
    
    * remove code smell
    
    * Merge branch 'release' into development
    
    * update the menu item page and clean up the navigation
    
    * create the delete menu repo method, service and controller
    
    * create the delete menu API
    
    * update food menu component to include item id
    
    * update food menu component to include item id
    
    * add the add menu item to cart functionality
    
    * implement add menu items to cart
    
    * fix build errors
    
    * fix code smell
    
    * create the item quantity button component
    
    * update the menu list ui
    
    * implement the add items to cart functionality
    
    * fix code smell
    
    * create the selected items summary
    
    * update cart reducer
    
    * update the add item to cart implementation
    
    * create folder structure for componsnts
    
    * implement the remove from cart functionality
    
    * implement the add to cart functionality
    
    * rename add and remove from cart to add and remove menu from cart
    
    * remove the menuid from foodmenu component
    
    * create the shopping component
    
    * implement display menu quantity, also display only menu items ata a time
    
    * prevent increase in total price if the menu quantity is 1
    
    * fix issues with totalprice calculations
    
    * fix issues with menu items reduction
    
    * fix the bug in shopping cart provider
    
    * remove menu price from global state
    
    * fix bug in remove menu from cart
    
    * fix error in menuItems calculations
    
    * update some names in the shoppingcart context
    
    * add the none button for food items
    
    * update remove item from cart method to calculate total price correctly
    
    * remove unused file
    
    * create the radio button
    
    * calculate order quantity
    
    * remove quantity count from checkout
    
    * Calculate cart total
    
    * disable add to cart buttons on page load
    
    * add development branch to github workflow, in order to track and build branches created from issues
    
    * fix code smells
    
    * enable the add to cart button on click none and addons
    
    * Incrementing cart quantity without addons
    
    * Fix: Add to Cart without incrementing the cart quantity throws an error
    
    * Create the summary modal and include error boundary in applocation
    
    * update error boundary url
    
    * add the edit and update button to the order summary page
    
    * add the call to action to clear cart
    
    * generate Ids for ordersummary
    
    * generate ordersummary ids
    
    * update the cart UI
    
    * create the localstorage utility functions
    
    * remove uuid package
    
    * remove crypto declaration
    
    * save state in local storage
    
    * persist cart state in local storage
    
    * Display list of car items
    
    * implement remove item from cart
    
    * refactor shopping cart context component, move type definition to a new folder

--- frontend/src/hooks/UseShoppingCart.tsx
+++ frontend/src/hooks/UseShoppingCart.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
-import { shoppingCartContext, shoppingCartProps } from "../contexts";
+import { shoppingCartContext } from "../contexts";
+import { shoppingCartProps } from "../contexts/shoppingCartTypes";
 
 export const useShoppingCart = (): shoppingCartProps => {
   return useContext(shoppingCartContext);
