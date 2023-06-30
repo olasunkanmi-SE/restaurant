58e35ee28fb8bdcf4db34e7aff49f839255dd9c6
ola <raymond.oyinlola@gmail.com>
2023-06-30 13:59:41 +0800

    create cart item domain and schema

--- backend/src/cart/cart-entity.interface.ts
+++ backend/src/cart/cart-entity.interface.ts
@@ -2,9 +2,8 @@ import { Types } from 'mongoose';
 import { ISelectedCartItem } from './cartItems/selected-cart-items-entity.interface';
 
 export interface ICartItem {
-  id: Types.ObjectId;
   menuId: Types.ObjectId;
   orderId: Types.ObjectId;
   total: number;
-  SelectedItem: ISelectedCartItem[];
+  selectedItems: ISelectedCartItem[];
 }
