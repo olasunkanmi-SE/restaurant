d2dfe38fb78e4646b49a8cb0d1efaaae371c1cdc
ola <raymond.oyinlola@gmail.com>
2023-09-11 09:53:20 +0800

    add cart items to order

--- backend/src/order/order.mapper.ts
+++ backend/src/order/order.mapper.ts
@@ -1,3 +1,4 @@
+import { CartItemMapper } from './../cart/cart-item.mapper';
 import { Injectable } from '@nestjs/common';
 import { IMapper } from 'src/domain';
 import { OrderDataModel } from 'src/infrastructure/data_access/repositories/schemas/order.schema';
@@ -6,9 +7,9 @@ import { AuditMapper } from 'src/audit';
 
 @Injectable()
 export class OrderMapper implements IMapper<Order, OrderDataModel> {
-  constructor(private readonly auditMapper: AuditMapper) {}
+  constructor(private readonly auditMapper: AuditMapper, private readonly cartItemMapper: CartItemMapper) {}
   toPersistence(entity: Order): OrderDataModel {
-    const { id, state, type, merchantId, total, quantity, discount, orderManagerId, audit } = entity;
+    const { id, state, type, merchantId, total, quantity, discount, orderManagerId, audit, cartItems } = entity;
     const {
       auditCreatedBy,
       auditCreatedDateTime,
@@ -25,6 +26,7 @@ export class OrderMapper implements IMapper<Order, OrderDataModel> {
       total,
       quantity,
       discount,
+      cartItems: cartItems.map((cartItem) => this.cartItemMapper.toPersistence(cartItem)),
       orderManagerId,
       auditCreatedBy,
       auditCreatedDateTime,
@@ -37,9 +39,19 @@ export class OrderMapper implements IMapper<Order, OrderDataModel> {
   }
 
   toDomain(model: OrderDataModel): Order {
-    const { state, type, merchantId, total, quantity, discount, orderManagerId, _id } = model;
+    const { state, type, merchantId, total, quantity, discount, orderManagerId, _id, cartItems } = model;
     const entity: Order = Order.create(
-      { state, type, merchantId, total, quantity, discount, orderManagerId, audit: this.auditMapper.toDomain(model) },
+      {
+        state,
+        type,
+        merchantId,
+        total,
+        quantity,
+        discount,
+        orderManagerId,
+        audit: this.auditMapper.toDomain(model),
+        cartItems: cartItems.map((cartItem) => this.cartItemMapper.toDomain(cartItem)),
+      },
       _id,
     ).getValue();
     return entity;
