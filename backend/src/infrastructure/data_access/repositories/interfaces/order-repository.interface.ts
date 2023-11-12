325edb9d9344515e5803e959de793e2d8bca1b0a
ola <raymond.oyinlola@gmail.com>
2023-11-12 14:47:16 +0800

    check for duplicate orders before creating one

--- backend/src/infrastructure/data_access/repositories/interfaces/order-repository.interface.ts
+++ backend/src/infrastructure/data_access/repositories/interfaces/order-repository.interface.ts
@@ -1,8 +1,10 @@
 import { Order } from 'src/order/order';
 import { OrderDataModel, OrderDocument } from '../schemas/order.schema';
 import { IGenericDocument } from 'src/infrastructure/database';
+import { CartItemDataModel } from '../schemas/cartItem.schema';
+import { Result } from 'src/domain';
 
 export interface IOrderRepository extends IGenericDocument<Order, OrderDocument> {
-  getOrders(): Promise<Order[]>;
-  createOrder(order: OrderDataModel): Promise<any>;
+  createOrder(order: OrderDataModel): Promise<Result<Order>>;
+  getDuplicateOrder(type: string, merchantId: string, cartItems: CartItemDataModel[]): Promise<boolean>;
 }
