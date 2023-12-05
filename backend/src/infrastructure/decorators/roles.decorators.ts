e5874319e94d3f30bbf8dbfca50a662c32adf36e
ola <raymond.oyinlola@gmail.com>
2023-12-05 22:17:17 +0800

    fix build errors

--- backend/src/infrastructure/decorators/roles.decorators.ts
+++ backend/src/infrastructure/decorators/roles.decorators.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
-import { ROLE_KEY, Role } from 'src/application';
+import { ROLE_KEY, Role } from '../../application';
 
 export const Roles = (...role: Role[]) => SetMetadata(ROLE_KEY, role);
